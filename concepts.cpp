782257b8f63ff9bb5ac1114d854a7aa50ec0fe39
Ugo Varetto <ugovaretto@gmail.com>
2020-07-23 21:16:18 +0800

    ...

--- concepts.cpp
+++ concepts.cpp
@@ -1,3 +1,5 @@
+//Author: Ugo Varetto
+
 #include <iostream>
 #include <concepts>
 #include <iterator>
@@ -74,10 +76,30 @@ void Q(H) {}
 template <typename F1, typename F2>
 concept divisible = requires(F1 f1, F2 f2) { f1 / f2 ;};
 
-static_assert(divisable<int, float>);
+static_assert(divisible<int, float>);
 
+template <typename T>
+concept number = is_integral<T>::value || is_floating_point<T>::value;
+
+//template <typename...ArgsT>
+//concept integers = (... && is_integral<ArgsT>::value)
+
+
+template <typename HeadT, typename...TailT>
+struct Head {
+    using Type = HeadT;
+};
+
+
+template <typename...NumbersT>
+constexpr typename Head<NumbersT...>::Type
+Sum(NumbersT...nums) requires (... && number<NumbersT>) {
+    using T = typename Head<NumbersT...>::Type;
+    return (... + T(nums));
+}
 
 int main(int argc, char const *argv[]) {
+    cout << Sum(1.0, 2, 4.f) << endl;
     S<int> s;
     //s.P(); //compilation error
     FizzBuzz<1,2,3,4,5,6,7,8,9,10,11,12,13,14,15>();
