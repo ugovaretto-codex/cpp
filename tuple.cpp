6fa19e537de7e976db348dd75f8a6a7e37026705
Ugo Varetto <ugovaretto@gmail.com>
2020-08-01 10:24:44 +0800

    Added tuple

--- tuple.cpp
+++ tuple.cpp
@@ -1,30 +1,9 @@
 // C++20 tuple implementation
 // author: Ugo Varetto
 
-// TODO: static_cast
-
 #include <iostream>
 
 using namespace std;
-
-// template <int...T>
-// struct Index {
-//     static constexpr int i[] = {T...};
-// };
-
-// template <int S, int...T>
-// struct Sequence : Sequence<S-1, T..., sizeof...(T)> {
-// };
-
-// template <int...T>
-// struct Sequence<0, T...> {
-//     using Type = Index<T...>;
-// };
-
-// template <int S>
-// struct MakeIndex {
-//     using Type = typename Sequence<S,0>::Type;
-// };
 
 //------------------------------------------------------------------------------
 template <typename H, typename... T>
@@ -33,7 +12,8 @@ struct Tuple : Tuple<T...> {
     Tuple(const H& h, const T&... t) : Tuple<T...>(t...), val(h) {}
     Tuple& operator=(const Tuple& other) {
         val = other.val;
-        Tuple<T...>::operator=((const Tuple<T...>&)other);
+        //cast to base
+        Tuple<T...>::operator=(static_cast<const Tuple<T...>&>(other));
         return *this;
     }
 };
@@ -48,6 +28,7 @@ struct Tuple<T> {
     }
 };
 
+//------------------------------------------------------------------------------
 template <int Position, typename H, typename... T>
 struct GetType {
     using Type = typename GetType<Position - 1, T...>::Type;
@@ -58,26 +39,41 @@ struct GetType<0, H, T...> {
     using Type = H;
 };
 
+//------------------------------------------------------------------------------
 template <int Pos, typename H, typename... ArgsT>
 const typename GetType<Pos, H, ArgsT...>::Type& Get(
     const Tuple<H, ArgsT...>& t) {
-    using Base = Tuple<ArgsT...>;
-    return Get<Pos - 1>((const Base&)t);
+    // cast to base
+    return Get<Pos - 1>(static_cast<const Tuple<ArgsT...>&>(t));
 }
 
 template <int Pos, typename H, typename... ArgsT>
 const typename GetType<Pos, H, ArgsT...>::Type& Get(
-    const Tuple<H, ArgsT...>& t) requires(Pos == 0) {
+    const Tuple<H, ArgsT...>& t) requires(Pos == 0) { //end of iteration
     return t.val;
 }
 
+template <int Pos, typename H, typename... ArgsT>
+typename GetType<Pos, H, ArgsT...>::Type& Get(
+    Tuple<H, ArgsT...>& t) {
+    // cast to base
+    return Get<Pos - 1>(static_cast<Tuple<ArgsT...>&>(t));
+}
+
+template <int Pos, typename H, typename... ArgsT>
+typename GetType<Pos, H, ArgsT...>::Type& Get(
+    Tuple<H, ArgsT...>& t) requires(Pos == 0) { //end of iteration
+    return t.val;
+}
+//------------------------------------------------------------------------------
 template <typename H, typename... ArgsT>
 bool operator==(const Tuple<H, ArgsT...>& t1, const Tuple<H, ArgsT...>& t2) {
+    using Base = Tuple<ArgsT...>;
     if constexpr (sizeof...(ArgsT) == 1)
         return t1.val == t2.val;
     else
         return t1.val == t2.val &&  // error without else part of constexpr
-               ((const Tuple<ArgsT...>&)t1 == (const Tuple<ArgsT...>&)t2);
+               static_cast<const Base&>(t1) == static_cast<const Base&>(t2);
 }
 
 template <typename H, typename... ArgsT>
@@ -91,10 +87,13 @@ std::ostream& operator<<(std::ostream& os, const Tuple<H, ArgsT...>& t) {
     return os;
 }
 
+//------------------------------------------------------------------------------
 int main(int argc, char const* argv[]) {
     Tuple<int, float, char> t = {2, 2.4f, 'c'};
     cout << t << endl;
     Tuple<int, float, char> t2 = {2, 2.4f, 'c'};
     cout << boolalpha << (t == t2) << endl;
+    Get<2>(t2) = 'X';
+    cout << Get<2>(t2) << endl;
     return 0;
 }
