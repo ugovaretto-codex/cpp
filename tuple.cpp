8f1f5ecc67d0582ac543446b5fb9a8d7d791f6a4
Ugo Varetto <ugovaretto@gmail.com>
2020-08-01 10:39:24 +0800

    static_cast

--- tuple.cpp
+++ tuple.cpp
@@ -9,10 +9,13 @@ using namespace std;
 template <typename H, typename... T>
 struct Tuple : Tuple<T...> {
     H val;
-    Tuple(const H& h, const T&... t) : Tuple<T...>(t...), val(h) {}
+    constexpr Tuple() = default;
+    constexpr Tuple(const H& h, const T&... t) : Tuple<T...>(t...), val(h) {}
+    constexpr Tuple(const Tuple& other)
+        : Tuple<T...>(static_cast<const Tuple<T...>&>(other)), val(other.val) {}
     Tuple& operator=(const Tuple& other) {
         val = other.val;
-        //cast to base
+        // cast to base
         Tuple<T...>::operator=(static_cast<const Tuple<T...>&>(other));
         return *this;
     }
@@ -21,7 +24,9 @@ struct Tuple : Tuple<T...> {
 template <typename T>
 struct Tuple<T> {
     T val;
-    Tuple(const T& v) : val(v) {}
+    constexpr Tuple() = default;
+    constexpr Tuple(const T& v) : val(v) {}
+    constexpr Tuple(const Tuple& other) = default;
     Tuple& operator=(const Tuple& other) {
         val = other.val;
         return *this;
@@ -49,25 +54,25 @@ const typename GetType<Pos, H, ArgsT...>::Type& Get(
 
 template <int Pos, typename H, typename... ArgsT>
 const typename GetType<Pos, H, ArgsT...>::Type& Get(
-    const Tuple<H, ArgsT...>& t) requires(Pos == 0) { //end of iteration
+    const Tuple<H, ArgsT...>& t) requires(Pos == 0) {  // end of iteration
     return t.val;
 }
 
 template <int Pos, typename H, typename... ArgsT>
-typename GetType<Pos, H, ArgsT...>::Type& Get(
-    Tuple<H, ArgsT...>& t) {
+typename GetType<Pos, H, ArgsT...>::Type& Get(Tuple<H, ArgsT...>& t) {
     // cast to base
     return Get<Pos - 1>(static_cast<Tuple<ArgsT...>&>(t));
 }
 
 template <int Pos, typename H, typename... ArgsT>
-typename GetType<Pos, H, ArgsT...>::Type& Get(
-    Tuple<H, ArgsT...>& t) requires(Pos == 0) { //end of iteration
+typename GetType<Pos, H, ArgsT...>::Type& Get(Tuple<H, ArgsT...>& t) requires(
+    Pos == 0) {  // end of iteration
     return t.val;
 }
 //------------------------------------------------------------------------------
 template <typename H, typename... ArgsT>
-bool operator==(const Tuple<H, ArgsT...>& t1, const Tuple<H, ArgsT...>& t2) {
+constexpr bool operator==(const Tuple<H, ArgsT...>& t1,
+                          const Tuple<H, ArgsT...>& t2) {
     using Base = Tuple<ArgsT...>;
     if constexpr (sizeof...(ArgsT) == 1)
         return t1.val == t2.val;
@@ -79,10 +84,10 @@ bool operator==(const Tuple<H, ArgsT...>& t1, const Tuple<H, ArgsT...>& t2) {
 template <typename H, typename... ArgsT>
 std::ostream& operator<<(std::ostream& os, const Tuple<H, ArgsT...>& t) {
     os << t.val << ' ';
-    if constexpr (sizeof...(ArgsT) >
-                  0)  // if *must* be evaluated at compile time
-                      // if not compiler will try to generate
-                      // the code for Tuple with zero elements
+    // "if" *must* be evaluated at compile time
+    // if not compiler will try to generate
+    // the code int the "if" body for Tuple with zero elements
+    if constexpr (sizeof...(ArgsT) > 0)
         os << static_cast<const Tuple<ArgsT...>&>(t);
     return os;
 }
